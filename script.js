acc4546fff21eef4216f91328c989bda3af7324e
btdrizzle <briandrake05@gmail.com>
2018-10-13 12:21:18 -0500

    upload 1

---
+++ script.js
@@ -0,0 +1,6 @@
+$('body').on('click', '#trivia-game', function() {
+    window.open('https://btdrizzle.github.io/TriviaGame/index.html');
+});
+$('body').on('click', '#giphy', function() {
+    window.open('https://btdrizzle.github.io/GifsNotJifs/index.html');
+});
